5c2542e78a02cce21b848ea9e591d8003b8455af
gomadango0113 <gomadango0113@gmail.com>
2025-10-27 20:44:57 +0900

    【追加】 赤と青色を定数に追加

--- GameUtil.h
+++ GameUtil.h
@@ -8,5 +8,7 @@ enum DrawType {
 
 const int COLOR_WHITE = GetColor(255, 255, 255);
 const int COLOR_BLACK = GetColor(0, 0, 0);
+const int COLOR_RED = GetColor(220, 20, 60);
+const int COLOR_BLUE = GetColor(0, 0, 140);
 
 void drawText(DrawType type, int x, int y, int size, const char* text, unsigned int text_color, unsigned int edge_color);
